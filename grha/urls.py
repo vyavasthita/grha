9495e20f213f389c23f219cd50fb451df4501af8
Dilip Kumar <diliplakshya@gmail.com>
2020-05-24 18:24:38 +1000

    updated url

--- grha/urls.py
+++ grha/urls.py
@@ -28,6 +28,5 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('tenancy', include('tenancy.tenancyurls')),
-    path('i18n/', include('django.conf.urls.i18n')),
     path('', include('home.homeurls')),
 )
