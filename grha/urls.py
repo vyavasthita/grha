714053a34b6aeef0e818999922a498f257b137f3
Dilip Kumar <diliplakshya@gmail.com>
2020-06-02 11:15:05 +1000

    added password reset

--- grha/urls.py
+++ grha/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,7 +28,19 @@ urlpatterns = [
     path('userauthentication/', include('userauthentication.userauthenticationurls', namespace='nsuserauthentication')),
     path('tenancy/', include('tenancy.tenancyurls')),
     path('i18n/', include('django.conf.urls.i18n')),
-    path('', include('home.homeurls', namespace='nshome'))
+    path('', include('home.homeurls', namespace='nshome')),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+                template_name="userauthentication/password_reset.html"), 
+                name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+                template_name="userauthentication/password_reset_confirm.html"), 
+                name='password_reset_confirm'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(
+                template_name="userauthentication/password_reset_done.html"), 
+                name='password_reset_done'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+                template_name="userauthentication/password_reset_complete.html"), 
+                name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
--- userauthentication/userauthenticationurls.py
+++ userauthentication/userauthenticationurls.py
@@ -24,6 +24,10 @@ app_name = 'userauthentication'
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name="userauthentication/login.html"), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name="userauthentication/logout.html"), name='logout'),
+    path('login/', auth_views.LoginView.as_view(
+                template_name="userauthentication/login.html"), 
+                name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+                template_name="userauthentication/logout.html"), 
+                name='logout'),
 ]
